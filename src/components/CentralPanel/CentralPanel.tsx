e633b2379e1f5933f81ea15ddd334b7cd8ee6d96
Max_Nikitenok <xxx250790@gmail.com>
2023-01-28 13:01:12 +0300

    feat: react icons & left panel navigation

--- src/components/CentralPanel/CentralPanel.tsx
+++ src/components/CentralPanel/CentralPanel.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './CentralPanel.module.css';
 import { tasksSelector } from '../../store/selectors';
 import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 const CentralPanel = () => {
   const tasks = useSelector(tasksSelector);
@@ -29,13 +30,51 @@ const CentralPanel = () => {
         <p>{t.name}</p>
       </div>
     ));
-  return (
-    <div className={style.CentralPanel}>
+
+  const Home = () => {
+    return (
+      <div className={style.home}>
+        <div>HOME</div>
+      </div>
+    );
+  };
+
+  const Boards = () => {
+    return (
       <div className={style.tasks}>
         <div>{notCompleteTasks}</div>
         <div>{inProgress}</div>
         <div>{completeTasks}</div>
       </div>
+    );
+  };
+
+  const Calendar = () => {
+    return (
+      <div className={style.tasks}>
+        <div>CALENDAR</div>
+      </div>
+    );
+  };
+
+  const NoMatch = () => {
+    return (
+      <div className={style.noMatch}>
+        <div>NO MATCH</div>
+      </div>
+    );
+  };
+
+
+
+  return (
+    <div className={style.CentralPanel}>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/boards" element={<Boards />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 };
--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './LeftPanel.module.css';
+import { RxHome, RxCalendar, RxDashboard } from 'react-icons/rx';
 
 const LeftPanel = () => {
   return (
     <div className={style.LeftPanel}>
-      <p>Home</p>
-      <p>My tasks</p>
-      <p>Calendar</p>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" className={({ isActive }) => isActive ? style.linkActive : style.link}>
+              <RxHome />
+              <span>Home</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/boards" className={({ isActive }) => isActive ? style.linkActive : style.link}>
+              <RxDashboard />
+              <span>Boards</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/calendar" className={({ isActive }) => isActive ? style.linkActive : style.link}>
+              <RxCalendar />
+              <span>Calendar</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
