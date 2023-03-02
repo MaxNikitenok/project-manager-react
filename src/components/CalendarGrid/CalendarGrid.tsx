1c0e58e94066e00ca15393e33ac2bd7518c87d50
Max_Nikitenok <xxx250790@gmail.com>
2023-03-02 23:10:53 +0300

    feat: week days

--- src/components/CalendarGrid/CalendarGrid.tsx
+++ src/components/CalendarGrid/CalendarGrid.tsx
@@ -8,29 +8,45 @@ export const CalendarGrid = (props: { startDay: { clone: () => any } }) => {
   const daysArray = [...Array(totalDays)].map(() => day.add(1, 'day').clone());
 
   return (
-    <div className={style.gridWrapper}>
-      {daysArray.map((dayItem) => (
-        <div
-          className={
-            dayItem.day() === 6 || dayItem.day() === 0
-              ? style.cellWeekEndWrapper
-              : style.cellWrapper
-          }
-          key={dayItem.unix()}
-        >
-          <div className={style.rowInCell}>
-            <div
-              className={
-                moment().isSame(dayItem, 'day')
-                  ? style.currentDay
-                  : style.dayWrapper
-              }
-            >
-              {dayItem.format('D')}
+    <>
+      <div className={style.weekDaysWrapper}>
+        {[...Array(7)].map((_, i) => (
+          <div className={style.weekDay} key={moment()
+            .day(i + 1)
+            .format('ddddd')}>
+            <div className={style.dayWrapper}>
+              {moment()
+                .day(i + 1)
+                .format('ddd')}
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+      <div className={style.gridWrapper}>
+        {daysArray.map((dayItem) => (
+          <div
+            className={
+              dayItem.day() === 6 || dayItem.day() === 0
+                ? style.cellWeekEndWrapper
+                : style.cellWrapper
+            }
+            key={dayItem.unix()}
+          >
+            <div className={style.rowInCell}>
+              <div
+                className={
+                  moment().isSame(dayItem, 'day')
+                    ? style.currentDay
+                    : style.dayWrapper
+                }
+              >
+                {dayItem.format('D')}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
